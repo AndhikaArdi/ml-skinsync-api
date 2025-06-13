6d4f170eca5fec0a087ac6c9ed67cec2378f6124
AndhikaArdi <127767610+AndhikaArdi@users.noreply.github.com>
2025-06-13 11:29:29 +0700

    changing port

--- predict_api.py
+++ predict_api.py
@@ -29,4 +29,7 @@ def predict():
     return jsonify({'type': skin_type})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
